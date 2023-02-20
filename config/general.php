e7a4f034d5882a0df4d2e3bacaeec4f0bba197e2
Jared Budlong <Jared@BlueFishDS.com>
2023-02-20 11:59:37 -0600

    Fixed the icon viewing inside the CP. Not sure of exactly how to replicate. Will research more in the future. Fairly certain is revolves around the project.yaml file and the following lines(57-60).
         settings:
           path: $DDEV_FILES_DIR
         type: craft\fs\Local
    -    url: $PRIMARY_SITE_URL
    +    url: '@uploads'

--- config/general.php
+++ config/general.php
@@ -11,15 +11,23 @@
 use craft\config\GeneralConfig;
 use craft\helpers\App;
 
+
+// Global settings
+   
 return GeneralConfig::create()
     // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
     ->defaultWeekStartDay(1)
     // Prevent generated URLs from including "index.php"
-    ->omitScriptNameInUrls()
+    ->omitScriptNameInUrls(true)
     // Enable Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
     ->devMode(App::env('DEV_MODE') ?? false)
     // Allow administrative changes
     ->allowAdminChanges(App::env('ALLOW_ADMIN_CHANGES') ?? false)
     // Disallow robots
     ->disallowRobots(App::env('DISALLOW_ROBOTS') ?? false)
+    ->aliases([
+        '@baseURL' => '$PRIMARY_SITE_URL',
+        '@uploads' => 'https://craft4-ddev-test.ddev.site:8443/assets/uploads',
+    ])
 ;
+
